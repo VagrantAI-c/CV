ba00425fd1ff58b5166c5285fdfabcad690a42a4
kirill.chirkov@home <kirill.chirkov@clouty.ru>
2020-09-01 13:54:47 +0300

    introduce crypto

--- commit/src/commit/index.ts
+++ commit/src/commit/index.ts
@@ -6,7 +6,7 @@ import { Schema } from './schema';
 export function commit(options: Schema): Rule {
     return () => {
         return chain([
-            executeCommand('npm.cmd', ['run', 'run:encode', `--password=${options.password}`]),
+            executeCommand('npm', ['run', 'run:encode', `--password=${options.password}`]),
             executeCommand('git', ['add', '.']),
             executeCommand('git', ['commit', '-m', `"${options.commitMessage}"`]),
             executeCommand('npm', ['run', 'run:decode', `--password=${options.password}`]),
@@ -17,7 +17,7 @@ export function commit(options: Schema): Rule {
 function executeCommand(command: string, args: string[] = []): Rule {
     return (host: Tree) => {
         return new Observable<Tree>((subscriber: Subscriber<Tree>) => {
-            const child = spawn(command, args, {stdio: 'inherit'});
+            const child = spawn(command, args, {stdio: 'inherit', shell: true});
             child.on('error', error => {
                 subscriber.error(error);
             });
